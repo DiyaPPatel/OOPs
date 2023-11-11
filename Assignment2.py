7ac5fa14789fd3410f7bd356b3a546a2e951cbaa
DiyaPPatel <patdp003@mymail.unisa.edu.au>
2023-11-11 20:24:17 +1030

    attributes added

--- Assignment2.py
+++ Assignment2.py
@@ -1,2 +1,21 @@
-Class Laboratory:
-    def __init__(self, ):
+class Alchemist:
+    def __init__(self, attack, strength, defense, magic, ranged, necromancy, Laboratory):
+        self.__attack = int
+        self.__strength = int
+        self.__defence = int
+        self.__magic = int
+        self.__ranged = int
+        self.__necromancy = int
+        self.__laboratory = Laboratory
+        self.__recipes = {}
+
+    def getLaboratory(self, Laboratory):
+        return self.__laboratory = Laboratory
+
+    def getRecipes():
+        return self.__recipes
+    
+    def mixPotion(self, recipe):
+
+    def drinkPotion():
+        
