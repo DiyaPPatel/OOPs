65ced42aa2595460208499c1f348f7630d0be083
DiyaPPatel <patdp003@mymail.unisa.edu.au>
2023-11-14 02:08:48 +1030

    methods defiened

--- Assignment2.py
+++ Assignment2.py
@@ -1,26 +1,37 @@
 class Alchemist:
     def __init__(self, attack, strength, defense, magic, ranged, necromancy, Laboratory):
-        self.__attack = int
-        self.__strength = int
-        self.__defence = int
-        self.__magic = int
-        self.__ranged = int
-        self.__necromancy = int
+        self.__attack = attack
+        self.__strength = strength
+        self.__defence = defense
+        self.__magic = magic
+        self.__ranged = ranged
+        self.__necromancy = necromancy
         self.__laboratory = Laboratory
         self.__recipes = {}
 
     def getLaboratory(self, Laboratory):
         self.__laboratory = Laboratory
 
-    def getRecipes():
+    def getRecipes(self):
         return self.__recipes
     
     def mixPotion(self, recipe):
 
     def drinkPotion():
+        pass
 
 
 class Laboratory:
+    def __init__(self):
+        self.__potions = []
+        self.__herbs = []
+        self.__catalysts = []
+
+    def mixPotion(self, name, type, stat, primaryIngredient, secondaryIngredient):
+        pass
+
+    def addReagent(reagent, amount):
+        pass
 
 
 class Potion:
