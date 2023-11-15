b1d21296adbff91bc61a79760b9fadc7f7c9d39b
DiyaPPatel <patdp003@mymail.unisa.edu.au>
2023-11-16 04:15:12 +1030

    classes defined

--- Assignment2.py
+++ Assignment2.py
@@ -1,8 +1,8 @@
 class Laboratory:
     def __init__(self):
-        self.potions = []
-        self.herbs = []
-        self.catalysts = []
+        self.__potions = []
+        self.__herbs = []
+        self.__catalysts = []
 
     def mixPotion(self, name, type, stat, primaryIngredient, secondaryIngredient):
         self.name = name
@@ -21,14 +21,14 @@ class Laboratory:
 
 class Alchemist:
     def __init__(self, attack, strength, defense, magic, ranged, necromancy, Laboratory):
-        self.attack = attack
-        self.strength = strength
-        self.defence = defense
-        self.magic = magic
-        self.ranged = ranged
-        self.necromancy = necromancy
-        self.laboratory = Laboratory
-        self.recipes = {}
+        self.__attack = attack
+        self.__strength = strength
+        self.__defence = defense
+        self.__magic = magic
+        self.__ranged = ranged
+        self.__necromancy = necromancy
+        self.__laboratory = Laboratory
+        self.__recipes = {}
 
     def getLaboratory(self, Laboratory):
         self.laboratory = Laboratory
@@ -44,13 +44,49 @@ class Alchemist:
 
 class Potion(Laboratory):
 
-    def __init__(self, name, stat, boost):
+    def __init__(self, potionName, stat, boost):
+        self.__potionName = potionName
+        self.__stat = stat
+        self.__boost = boost
+
+    def calculateBoost(self):
+        pass
+
+    def getName(self):
+        return self.__potionName
+    
+    def getStat(self):
+        return self.__stat
+
+    def getBoost(self):
+        return self.__boost
+    
+    def setBoost(self):
+        pass
         
 
 class SuperPotion(Potion):
-    def 
+    def __init__(self, potionName, stat, boost, herb, catalyst):
+        super().__init__(potionName, stat, boost)
+        self.__herb = herb
+        self.__catalyst = catalyst
+
+    def calculateBoost(self):
+        super().calculateBoost
+
+    def getHerb(self):
+        return self.__herb
+    
+    def getCatalyst(self):
+        return self.__catalyst
+    
+
 
 class ExtremePotion(Potion):
+    def __init__(self, name, stat, boost, reagent, potion):
+        super().__init__(name,stat,boost)
+        self.__reagent = reagent
+        self.__potion = potion
 
 class Reagent:
 
